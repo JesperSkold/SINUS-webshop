9b35ba91b1d038dafed8f40a57ff6378c72d36b3
Artur <artur.lindstrom@gmail.com>
2022-02-18 11:02:31 +0100

    Add basic styling to reusable button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,55 @@
 <template>
   <div id="app">
+    <button class="sinus-button">tuttar</button>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Mukta+Malar:wght@400;700&display=swap');
+h1, h2, h3, h4, h5 {
+  font-family: 'Montserrat', sans-serif;
+}
+h1 {
+  font-size: 6rem
+}
+h2 {
+  font-size: 4rem
+}
+h3 {
+  font-size: 3rem
+}
+h4 {
+  font-size: 2rem
+}
+p, button {
+  font-family: 'Mukta Malar', sans-serif;
+}
+button {
+  font-size: .875rem
+  
+}
+  .sinus-button{
+    background: #003827;
+    color: White;
+    border-radius: 4px;
+    padding: 1rem 2.5rem;
+    border: none;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  .sinus-button:active{
+    background: #015239;
+  }
+
+  .sinus-button:hover{
+    box-shadow: -1px 3px 6px 0px rgba(0, 0, 0, 0.4);
+
+  }
+</style>

c0c05f1a3433977472c5381773627c3ca7514e9b
Skold <jesper.olsson@iths.se>
2022-02-21 12:01:27 +0100

    Add account view

--- src/components/MyAccount.vue
+++ src/components/MyAccount.vue
@@ -1,9 +1,85 @@
 <template>
-  <div class="my-account"></div>
+  <div class="my-account">
+    <section class="account-info">
+      <h3>MY ACCOUNT</h3>
+      <!--iterate over account /api/me -->
+      <h5>GÖRAN ARVIDSSON</h5>
+      <!--.name, etc -->
+      <h5>STOCKHOLM</h5>
+      <h5>SOLVÄGEN 25</h5>
+      <h5>137 58</h5>
+    </section>
+    <section class="account-orders">
+      <h3>ORDER HISTORY</h3>
+      <!-- iterate over /api/orders -->
+      <div class="orders">
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+        <div>
+          <p>Gretas Fury</p>
+          <p>$220</p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.my-account {
+  display: flex;
+  justify-content: center;
+  padding: 5rem;
+  gap: 20vh;
+}
+
+.account-info {
+  h3 {
+    text-decoration: underline;
+  }
+}
+
+.orders {
+  height: 35vh;
+  min-width: 15vw;
+  overflow: auto;
+
+  div {
+    display: flex;
+    gap: 1rem;
+  }
+}
+</style>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -12,7 +12,7 @@ export default {
   components: { LoginForm, MyAccount },
   data() {
     return {
-      loggedIn: false,
+      loggedIn: true,
     };
   },
 };

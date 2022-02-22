aec59dff8742c27ff0dd467e5b62145ba1fc940e
aMeijerr <caa.meijer@gmail.com>
2022-02-22 13:12:13 +0100

    Add product modal

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -17,30 +17,43 @@
   <article class="product">
     <div class="product-image-container">
       <router-link to="">
-        <img class="shopping-cart" src="../assets/shopping-cart.svg" alt="Shopping cart">
+        <img
+          class="shopping-cart"
+          src="../assets/shopping-cart.svg"
+          alt="Shopping cart"
+        />
       </router-link>
       <router-link to="">
-      <img :src="'http://localhost:5000' + '/images/'+ product.imgFile" class="product-image" height="200" />
+        <img
+          :src="'http://localhost:5000' + '/images/' + product.imgFile"
+          class="product-image"
+          height="200"
+          @click="showProductModal(product.id)"
+        />
       </router-link>
     </div>
     <router-link to="/product/:category/:id">
-      <p class="product-title">{{product.title}}</p>
+      <p class="product-title">{{ product.title }}</p>
     </router-link>
-    <p class="product-price">${{product.price}}</p>
+    <p class="product-price">${{ product.price }}</p>
   </article>
 </template>
 
-
 <script>
 export default {
-  props: {product: Object},
+  props: { product: Object },
   computed: {
-    image(){
-      return 'http://localhost:5000/images/' + this.product.imgFile
-    }
-  }
-
-}
+    image() {
+      return "http://localhost:5000/images/" + this.product.imgFile;
+    },
+  },
+  methods: {
+    showProductModal(id) {
+      this.$store.dispatch("showProductModal");
+      this.$emit("sendId", id);
+    },
+  },
+};
 </script>
 
 <style scoped lang:scss>
@@ -59,23 +72,21 @@ export default {
 .shopping-cart {
   position: absolute;
   right: 2rem;
-  transition: 100ms ease-in
+  transition: 100ms ease-in;
 }
 
 .shopping-cart:hover {
   transform: scale(1.1);
-  transition: 100ms ease-in
+  transition: 100ms ease-in;
 }
 
-.product-title, .product-price {
+.product-title,
+.product-price {
   text-align: center;
   margin: 0;
 }
 
 .product-price {
-  color: #881616
+  color: #881616;
 }
-
-
-
 </style>

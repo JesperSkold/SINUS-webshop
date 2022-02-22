fbffe679f21e78496b2f8ba98af23844655ef238
aMeijerr <caa.meijer@gmail.com>
2022-02-22 15:13:46 +0100

    Add product modal and logic in store

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,51 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/HomeView.vue'
-import Products from '../views/ProductsView.vue'
-import ApparelView from '../views/ApparelView.vue'
-import SingleProduct from '../views/SingleProductView.vue'
-import Account from '../views/AccountView.vue'
-import Checkout from '../views/CheckoutView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/HomeView.vue";
+import Products from "../views/ProductsView.vue";
+import ApparelView from "../views/ApparelView.vue";
+import SingleProduct from "../views/SingleProductView.vue";
+import Account from "../views/AccountView.vue";
+import Checkout from "../views/CheckoutView.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/products',
-    name: 'Products',
-    component: Products
+    path: "/products",
+    name: "Products",
+    component: Products,
   },
   {
-    path: '/apparel',
-    name: 'Apparel',
-    component: ApparelView
+    path: "/apparel",
+    name: "Apparel",
+    component: ApparelView,
   },
   {
-    path: '/singleProduct',
-    name: 'SingleProduct',
-    component: SingleProduct
+    path: "/singleProduct",
+    name: "SingleProduct",
+    component: SingleProduct,
   },
   {
-    path: '/account',
-    name: 'Account',
-    component: Account
+    path: "/account",
+    name: "Account",
+    component: Account,
   },
   {
-    path: '/checkout',
-    name: 'Checkout',
-    component: Checkout
+    path: "/checkout",
+    name: "Checkout",
+    component: Checkout,
   },
-  
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;

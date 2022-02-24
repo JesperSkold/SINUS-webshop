ca06ed0f59778f4d118bd2b09048f73aef9e11d4
Jesper <jesper.olsson@iths.se>
2022-02-24 16:55:54 +0100

    Add order history functionallity for account view

--- src/components/MyAccount.vue
+++ src/components/MyAccount.vue
@@ -1,69 +1,95 @@
 <template>
-  <div class="my-account">
-    <section class="account-info" v-if="accountInfo">
-      <h3>MY ACCOUNT</h3>
-      <h5>{{accountInfo.name}}</h5>
-      <h5>{{accountInfo.email}}</h5>
-      <h5>{{accountInfo.address.street}}</h5>
-      <h5>{{accountInfo.address.zip}}</h5>
-      <h5>{{accountInfo.address.city}}</h5>
-    </section>
-    <section class="account-orders">
-      <h3>ORDER HISTORY</h3>
-      <!-- iterate over /api/orders -->
-      <div class="orders">
-        <div v-for="orders in orderHistory" :key="orders.id">
-            {{orders.createdAt.slice(0, 10)}}
-            Order ID:{{orders.id}}
-          <div v-for="items in orders.items" :key="items.id">
-            Price:{{items.price+items.price}}
-            Amount:{{items.amount}}
-          </div> 
-        </div>
-      </div>
-    </section>
-  </div>
+	<div class="my-account">
+		<section class="account-info" v-if="accountInfo">
+			<h3>MY ACCOUNT</h3>
+			<h5>{{ accountInfo.name }}</h5>
+			<h5>{{ accountInfo.email }}</h5>
+			<h5>{{ accountInfo.address.street }}</h5>
+			<h5>{{ accountInfo.address.zip }}</h5>
+			<h5>{{ accountInfo.address.city }}</h5>
+		</section>
+		<section class="account-orders">
+			<h3>ORDER HISTORY</h3>
+			<div class="orders" v-for="item in uniqueOrder" :key="item.id">
+				<h4>OrderID: {{ item.id }}</h4>
+				<p>
+					Total price: <span>${{ item.sum }}</span>
+				</p>
+				<p>Order date: {{ item.date }}</p>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
 export default {
-  computed: {
-    accountInfo(){
-      return this.$store.state.accountInfo
-    },
-    orderHistory(){
-      return this.$store.state.orderHistory
-    }
-  }, //fetch products, implement find function against products and render product name with id
-    async mounted(){
-      await this.$store.dispatch('getAccountInfo')
-      await this.$store.dispatch('getOrderHistory')
-    }
+	computed: {
+		uniqueOrder() {
+			let orders = [];
+			let orderInfo = null;
+			let sum = 0;
+			for (let i = 0; i < this.orderHistory.length; i++) {
+				orderInfo = { date: this.orderHistory[i].createdAt.slice(0, 10), id: this.orderHistory[i].id };
+				for (const elem of this.orderHistory[i].items) {
+					sum += elem.price * elem.amount;
+				}
+				orders.push({ ...orderInfo, sum });
+				sum = 0;
+			}
+			return orders;
+		},
+		accountInfo() {
+			return this.$store.state.accountInfo;
+		},
+		orderHistory() {
+			return this.$store.state.orderHistory;
+		},
+	},
+	async mounted() {
+		await this.$store.dispatch("getAccountInfo");
+		await this.$store.dispatch("getOrderHistory");
+	},
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/styles/_global-colors.scss";
 .my-account {
-  display: flex;
-  justify-content: center;
-  padding: 5rem;
-  gap: 20vh;
+	display: flex;
+	justify-content: center;
+	padding: 5rem;
+	gap: 20vh;
 }
 
 .account-info {
-  h3 {
-    text-decoration: underline;
-  }
+	h3 {
+		text-decoration: underline;
+	}
+}
+
+.account-orders {
+	padding: 0 1rem;
+	height: 50vh;
+	overflow-y: auto;
 }
 
 .orders {
-  min-width: 15vw;
-  overflow: auto;
-
-  div {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
+  p:nth-child(2) {
+    display: inline;
+    span {
+      color: $price-color;
+    }
   }
+	p:nth-child(3) {
+		margin-bottom: 2rem;
+		border-bottom: 1px solid black;
+	}
+	p {
+		display: flex;
+		flex-direction: column;
+	}
+	h4 {
+		margin: 1rem 0;
+	}
 }
 </style>
